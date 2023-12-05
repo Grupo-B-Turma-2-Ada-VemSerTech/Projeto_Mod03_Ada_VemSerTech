0337c92f29689775865264b5f4d58a93335d6b5a
Leticia Santos <leticiaholsback@gmail.com>
2023-12-05 08:37:42 -0300

    CÓDIGO CORRIGIDO E CRIADO NO POSTGRE

--- loja_criacao_tabelas.sql
+++ loja_criacao_tabelas.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE loja_db;
 
 CREATE TABLE loja_db.public.fornecedor(
-  FornecedorID SERIAL SMALLINT PRIMARY KEY,
+  FornecedorID SERIAL PRIMARY KEY,
   Nome VARCHAR (80) NOT NULL,
   CNPJ CHAR(14) NOT NULL,
   Telefone SMALLINT,
@@ -9,56 +9,52 @@ CREATE TABLE loja_db.public.fornecedor(
   Num_predial SMALLINT,
   Bairro VARCHAR (20),
   Cidade VARCHAR (30),
-  Uf VARCHAR(2)
+  UF VARCHAR(2),
 
-  constraint unique_cnpj_fornecedor unique(CNPJ)
+  CONSTRAINT unique_cnpj_fornecedor UNIQUE (CNPJ)
 );
 
 CREATE TABLE loja_db.public.produto(
-  produtoid SERIAL SMALLINT PRIMARY KEY,
+  produtoid SERIAL PRIMARY KEY,
   fornecedorid SMALLINT,
   categoria VARCHAR (30),
   Preco DECIMAL(10, 2) NOT NULL,
-  quantidade SMALLINT
+  quantidade SMALLINT,
 
-  FOREIGN KEY (fornecedorID) REFERENCES fornecedor(fornecedorID),
+  FOREIGN KEY (fornecedorID) REFERENCES fornecedor(fornecedorID)
 );
 
 CREATE TABLE vendedor(
-  vendedorID SERIAL SMALLINT PRIMARY KEY,
-  Fname varchar(10) NOT NULL,
-  Minit char(3),
-  Lname varchar(10)
+  vendedorID SERIAL PRIMARY KEY,
+  Nome_vendedor varchar(30) NOT NULL
 );
 
-CREATE TABLE loja.db.public.cliente(
-  clienteID SERIAL SMALLINT PRIMARY KEY,
-  Fname varchar(10) NOT NULL, -- first name
-  Minit char(3), -- middle initial
-  Lname varchar(10), -- last name
+CREATE TABLE loja_db.public.cliente(
+  clienteID SERIAL PRIMARY KEY,
+  Nome_cliente varchar(40) NOT NULL,
   CPF char(11) not null,
-  Genero - VARCHAR(1) CHECK (Genero IN ('M', 'F')), -- Restrição M ou F
-  Idade - SMALLINT,
-  Rua- VARCHAR(30),
-  "Nº residencial" SMALLINT,,
-  Bairro - VARCHAR (20),
-  Cidade - VARCHAR (30),
-  CEP - INTEGER,
-  UF - VARCHAR(2),
+  Genero VARCHAR(1) CHECK (Genero IN ('M', 'F')), -- Restrição M ou F
+  Idade SMALLINT,
+  Rua VARCHAR(30),
+  "Nº residencial" SMALLINT,
+  Bairro VARCHAR (20),
+  Cidade VARCHAR (30),
+  CEP INTEGER,
+  UF VARCHAR(2),
 
   constraint unique_cpf_client unique(CPF)
 );
 
 
 CREATE TABLE loja_db.public.vendas(
-  vendaid SERIAL INTEGER PRIMARY KEY,
+  vendaid SERIAL PRIMARY KEY,
   produtoid SMALLINT,
   clienteid SMALLINT,
   valor DOUBLE PRECISION NOT NULL,
   tipo_Pagamento VARCHAR(30) NOT NULL,
   cidade_venda VARCHAR(50) NOT NULL,
-  data TIMESTAMP
+  data TIMESTAMP,
   
   FOREIGN KEY (produtoID) REFERENCES produto(produtoID),
-  FOREIGN KEY (clienteID) REFERENCES cliente(clienteID),
+  FOREIGN KEY (clienteID) REFERENCES cliente(clienteID)
 );
