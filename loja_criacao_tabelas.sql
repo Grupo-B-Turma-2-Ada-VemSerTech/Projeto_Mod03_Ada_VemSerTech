13527dd837230bf7ca421aa32cb7f12a3d06a9fc
Leticia Santos <leticiaholsback@gmail.com>
2023-12-06 20:10:17 -0300

    Padronizaçao de nome das tabelas e colunas

--- loja_criacao_tabelas.sql
+++ loja_criacao_tabelas.sql
@@ -1,61 +1,59 @@
 CREATE DATABASE loja_db;
 
-CREATE TABLE loja_db.public.fornecedor(
+CREATE TABLE loja_db.public.Cliente(
+  ClienteID SERIAL PRIMARY KEY,
+  Nome_cliente varchar(40) NOT NULL,
+  Cpf char(11) not null,
+  Genero VARCHAR(1) CHECK (Genero IN ('M', 'F')), -- Restrição M ou F
+  Idade SMALLINT,
+  Rua VARCHAR(30),
+  "Nº residencial" SMALLINT,
+  Bairro VARCHAR (20),
+  Bidade VARCHAR (30),
+  Cep INTEGER,
+  Uf VARCHAR(2),
+
+  constraint unique_cpf_client unique(CPF)
+);
+CREATE TABLE loja_db.public.Fornecedor(
   FornecedorID SERIAL PRIMARY KEY,
   Nome VARCHAR (80) NOT NULL,
-  cnpj CHAR(14) NOT NULL,
-  telefone VARCHAR(20),
-  rua VARCHAR(30),
-  num_predial SMALLINT,
-  bairro VARCHAR (20),
-  cidade VARCHAR (30),
-  uf VARCHAR(2),
+  Cnpj CHAR(14) NOT NULL,
+  Telefone VARCHAR(20),
+  Rua VARCHAR(30),
+  Num_predial SMALLINT,
+  Bairro VARCHAR (20),
+  Cidade VARCHAR (30),
+  Uf VARCHAR(2),
 
   CONSTRAINT unique_cnpj_fornecedor UNIQUE (CNPJ)
 );
 
-CREATE TABLE loja_db.public.produto(
-  produtoid SERIAL PRIMARY KEY,
-  nome VARCHAR(150),
-  fornecedorid SMALLINT,
-  categoria VARCHAR (30),
-  preco DECIMAL(10, 2) NOT NULL,
-  quantidade SMALLINT,
+CREATE TABLE loja_db.public.Produto(
+  ProdutoID SERIAL PRIMARY KEY,
+  Nome VARCHAR(150),
+  FornecedorID SMALLINT,
+  Categoria VARCHAR (30),
+  Preco DECIMAL(10, 2) NOT NULL,
+  Quantidade SMALLINT,
 
-  FOREIGN KEY (fornecedorID) REFERENCES fornecedor(fornecedorID)
+  FOREIGN KEY (FornecedorID) REFERENCES Fornecedor(FornecedorID)
 );
 
-CREATE TABLE vendedor(
-  vendedorID SERIAL PRIMARY KEY,
-  nome_vendedor varchar(30) NOT NULL
-);
-
-CREATE TABLE loja_db.public.cliente(
-  clienteID SERIAL PRIMARY KEY,
-  nome_cliente varchar(40) NOT NULL,
-  cpf char(11) not null,
-  genero VARCHAR(1) CHECK (Genero IN ('M', 'F')), -- Restrição M ou F
-  idade SMALLINT,
-  rua VARCHAR(30),
-  "Nº residencial" SMALLINT,
-  bairro VARCHAR (20),
-  cidade VARCHAR (30),
-  cep INTEGER,
-  uf VARCHAR(2),
-
-  constraint unique_cpf_client unique(CPF)
-);
-
-
-CREATE TABLE loja_db.public.vendas(
-  vendaid SERIAL PRIMARY KEY,
-  produtoid SMALLINT,
-  clienteid SMALLINT,
-  valor DOUBLE PRECISION NOT NULL,
-  tipo_Pagamento VARCHAR(30) NOT NULL,
-  cidade_venda VARCHAR(50) NOT NULL,
+CREATE TABLE loja_db.public.Vendas(
+  VendaID SERIAL PRIMARY KEY,
+  ProdutoID SMALLINT,
+  ClienteID SMALLINT,
+  VendaID DOUBLE PRECISION NOT NULL,
+  Tipo_Pagamento VARCHAR(30) NOT NULL,
+  Cidade_venda VARCHAR(50) NOT NULL,
   data TIMESTAMP,
   
-  FOREIGN KEY (produtoID) REFERENCES produto(produtoID),
-  FOREIGN KEY (clienteID) REFERENCES cliente(clienteID)
+  FOREIGN KEY (ProdutoID) REFERENCES Croduto(ProdutoID),
+  FOREIGN KEY (ClienteID) REFERENCES Cliente(ClienteID)
+);
+
+CREATE TABLE Vendedor(
+  VendedorID SERIAL PRIMARY KEY,
+  Nome_vendedor varchar(30) NOT NULL
 );
