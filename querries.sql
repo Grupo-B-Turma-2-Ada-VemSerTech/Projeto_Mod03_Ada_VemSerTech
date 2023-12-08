9fe2701e6abfc3f86fd0289048454af419e4eee1
Amanda Rodrigues de Souza <123509140+RSAmanda@users.noreply.github.com>
2023-12-08 16:13:41 -0300

    Create querries.sql
    criação de uma view q calcula o preço por produto vendido (preço * quantidade)

---
+++ querries.sql
@@ -0,0 +1,16 @@
+-- Cálculo do preço por produto
+DROP VIEW IF EXISTS vw_produto_total;
+
+CREATE VIEW vw_produto_total AS
+SELECT vendas.vendaid AS "ID da Venda",
+	   produto.nome_produto AS "Nome do Produto",
+	   produto.preco AS "Preço",
+	   produto_vendido.quantidade "Quantidade Vendida",
+	   produto.preco * produto_vendido.quantidade AS "Preço Total"
+	   
+	FROM loja_db.public.produto_vendido
+	INNER JOIN loja_db.public.produto
+		ON produto.produtoid = produto_vendido.produtoid
+	INNER JOIN loja_db.public.vendas
+		ON vendas.vendaid = produto_vendido.vendaid;
+SELECT * FROM vw_produto_total
