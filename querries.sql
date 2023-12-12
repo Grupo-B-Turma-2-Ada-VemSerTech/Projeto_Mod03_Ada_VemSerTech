1542e3cda28a5c6e14afd28b65e5d5809f640f39
Amanda <amandars@id.uff.br>
2023-12-12 15:25:46 -0300

    correção de detalhes nas querries

--- querries.sql
+++ querries.sql
@@ -13,7 +13,7 @@ SELECT vendas.vendaid AS "ID da Venda",
 		ON produto.produtoid = produto_vendido.produtoid
 	INNER JOIN loja_db.public.vendas
 		ON vendas.vendaid = produto_vendido.vendaid;
-SELECT * FROM vw_produto_total
+--SELECT * FROM vw_produto_total
 
 -- Cálculo to total por compra
 DROP VIEW IF EXISTS vw_venda_total;
@@ -22,7 +22,7 @@ SELECT vw_produto_total."ID da Venda" AS vendaid,
 		SUM("Preço Total") AS valor_total
 FROM vw_produto_total
 GROUP BY vw_produto_total."ID da Venda"
-ORDER BY vw_produto_total."ID da Venda"
+ORDER BY vw_produto_total."ID da Venda";
 
 -- View da tabela de venda com o valor total da venda calculada
 CREATE VIEW vw_valor_venda AS
@@ -34,7 +34,7 @@ CREATE VIEW vw_valor_venda AS
 			vendas.data
 	FROM vendas
 		INNER JOIN vw_venda_total
-		ON vendas.vendaid = vw_venda_total.vendaid
+		ON vendas.vendaid = vw_venda_total.vendaid;
 
 
 -- para mostrar o nome do vendendor, nome do cliente, o id da venda e o valor total
@@ -44,15 +44,18 @@ CREATE VIEW vw_vendas_vendedor AS
 SELECT vendedor.nome_vendedor AS "Nome Vendedor",
 	   cliente.nome_cliente AS "Nome Cliente",
 	   vendas.vendaid AS "Venda ID",
-	   vendas.valor_total AS "Total da Compra"
+	   vw_venda_total.valor_total AS "Total da Compra"
 	   
 	FROM loja_db.public.vendas
+	INNER JOIN vw_venda_total
+		ON vendas.vendaid = vw_venda_total.vendaid
 	INNER JOIN loja_db.public.vendedor
 		ON vendas.vendedorid = vendedor.vendedorid
 	INNER JOIN loja_db.public.cliente
 		ON vendas.clienteid = cliente.clienteid;
-SELECT * FROM vw_vendas_vendedor
-order by "Nome Vendedor" 
+--SELECT * FROM vw_vendas_vendedor
+--order by "Nome Vendedor" 
 
 -- quantidade de itens por categoria
+CREATE VIEW vw_item_categoria AS
 SELECT categoria, count(*) AS "Quantidade de Itens" FROM produto group by produto.categoria
